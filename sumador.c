c6228fc4e1564c7b3eba07808c6c29c97281646e
Miguel Silva C. <correo@mqail.com>
2024-04-18 16:21:57 -0300

    Sumador v1.3

--- sumador.c
+++ sumador.c
@@ -4,12 +4,14 @@
 
 int main(void)
 {
-    int num1, num2, suma;
-    p("Ingrese un valor: ");
+    int num1, num2, num3, suma;
+    p("Ingrese primer valor: ");
     s("%d", &num1);
-    p("Ingrese otro valor: ");
+    p("Ingrese segundo valor: ");
     s("%d", &num2);
-    suma = num1 + num2;
-    p("%d + %d = %d", num1,num2,suma);
+    p("Ingrese tercer valor: ");
+    s("%d", &num3);
+    suma = num1 + num2 + num3;
+    p("%d + %d + %d = %d", num1,num2, num3,suma);
     return 0;
 }
