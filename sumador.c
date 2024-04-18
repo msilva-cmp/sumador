0b443535a339165453dd446abc0ef985ccea1735
Miguel Silva C. <correo@mqail.com>
2024-04-18 16:28:48 -0300

    Sumador retrocedido

--- sumador.c
+++ sumador.c
@@ -4,14 +4,12 @@
 
 int main(void)
 {
-    int num1, num2, num3, suma;
+    int num1, num2, suma;
     p("Ingrese primer valor: ");
     s("%d", &num1);
     p("Ingrese segundo valor: ");
     s("%d", &num2);
-    p("Ingrese tercer valor: ");
-    s("%d", &num3);
-    suma = num1 + num2 + num3;
-    p("%d + %d + %d = %d", num1,num2, num3,suma);
+    suma = num1 + num2;
+    p("%d + %d = %d", num1,num2, suma);
     return 0;
 }
